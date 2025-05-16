222184bac8231e6dcf8a369966810f13e86a8a83
shobbanshbn <shobanshbn@gmail.com>
2025-05-16 15:51:41 +0530

    Completed

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Memory Game Task
+    Dice Game Creation Using HTML, CSS, JS DOM
+
+# TechStacks
+    1. HTML
+    2. CSS
+    3. JS DOM
+
+# How To Play
+    1. One Players can participate.
+    2. Game logic is Match the card face to win the game.
+    3. Flip the any one card to reveal the front face after select another one card.
+    4. If Both are same face it will open and move to select other face, or other wise different it will close and 
+       move to same position until match the card face.
+    5. Once a player Match the all card face will win the game.
+    6. Then the game will be resetted you can start playing again.
+
--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 
 const game = document.getElementById("game")
+const alert = document.getElementById("alert")
 
 const symbols = ["😍","😍","🤣","🤣","😒","😒","😎","😎","😬","😬","🤬","🤬","😨","😨","🤑","🤑"];
 const randomSymbols = symbols.sort(()=> (Math.random()> .5) ? 2 : -1);
 for ( var i=0; i<symbols.length; i++){
-    // const box = document.getElementById("item")
+    
     const box = document.createElement("div")
     box.className = "item";
     box.innerHTML = randomSymbols[i]
+
+    box.onclick = function(){
+       box.classList.add("boxOpen")
+       setTimeout(function(){
+        if(document.querySelectorAll(".boxOpen").length > 1){
+            if(document.querySelectorAll(".boxOpen")[0].innerHTML == 
+                document.querySelectorAll(".boxOpen")[1].innerHTML){
+                    document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch")
+                    document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch")
+
+                    document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen")
+                    document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen")
+
+                    if(document.querySelectorAll(".boxMatch").length == symbols.length){
+                        // alert("You Won the Game🎉🎊")
+                        
+                        alert.innerHTML = "🎊..You Won The Game..🎊"
+                        
+                    }
+                    }else{
+                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen")
+                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen")
+                    }
+                }
+           },500)
+    }
+
     document.querySelector(".game").append(box);
-}
+        
+    };
